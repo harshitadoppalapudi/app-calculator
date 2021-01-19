ef31869afc5fc4c4677246df601b9089f2b90638
harshita <harshita1142@gmail.com>
2021-01-19 12:37:54 +0530

    initial commiy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-harshita';
+  number1='';
+  number2='';
+  add:any='';
+  subtract:any='';
+  multiply:any='';
+  divide:any='';
+  modular:any='';
+
+  Add()
+  {
+    this.add=Number(this.number1)+Number(this.number2);
+  }
+  Subtract()
+  {
+    this.add=Number(this.number1)-Number(this.number2);
+  }
+  Multiply()
+  {
+    this.add=Number(this.number1)*Number(this.number2);
+  }
+  Divide()
+  {
+    this.add=Number(this.number1)/Number(this.number2);
+  }
+  Modular()
+  {
+    this.add=Number(this.number1)%Number(this.number2);
+  }
+  Clear(){
+    this.number1='';
+    this.number2='';
+    this.add='';
+    this.subtract='';
+    this.multiply='';
+    this.divide='';
+    this.modular='';
 }
+}
+
